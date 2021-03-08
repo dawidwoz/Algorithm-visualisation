e7ba0eed4d60a14b4d28bba025e413c340ea760c
Dawid Wozniak <daw79@aber.ac.uk>
2021-03-08 22:21:25 +0000

    fix(router): Remove Angular router

--- apps/visualisation/src/app/app.component.ts
+++ apps/visualisation/src/app/app.component.ts
@@ -1,13 +1,8 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
 
 @Component({
   selector: 'major-project-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  constructor(router: Router) {
-    router.navigate(['/welcome']);
-  }
-}
+export class AppComponent {}
--- apps/visualisation/src/app/app.module.ts
+++ apps/visualisation/src/app/app.module.ts
@@ -2,7 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app-routing.module';
 import { StackComponent } from './stack/stack.component';
 import { QueueComponent } from './queue/queue.component';
 import { PriorityQueueComponent } from './priority-queue/priority-queue.component';
@@ -32,7 +31,6 @@ const icons = {
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    AppRoutingModule,
     StackModule,
     BinaryTreeModule,
     HashFunctionModule,
