cc27c9c1a7f0604e13b077dd359a321928b71d85
Dawid Wozniak <daw79@aber.ac.uk>
2021-04-24 04:02:32 +0100

    temp: Hide markov model

--- apps/visualisation/src/app/app-routing.module.ts
+++ apps/visualisation/src/app/app-routing.module.ts
@@ -13,7 +13,6 @@ export const RoutesName = {
   'priority-queue': 'priority-queue',
   'hash-function': 'hash-function',
   'binary-search-tree': 'binary-search-tree',
-  'hidden-markov-model': 'hidden-markov-model',
   default: 'stack'
 };
 
@@ -23,7 +22,7 @@ const routes: Routes = [
   { path: RoutesName['priority-queue'], component: PriorityQueueComponent },
   { path: RoutesName['hash-function'], component: HashFunctionComponent},
   { path: RoutesName['binary-search-tree'], component: BinarySearchTreeComponent },
-  { path: RoutesName['hidden-markov-model'], component: HiddenMarkovModelComponent },
+  { path: 'hidden-markov-model', component: HiddenMarkovModelComponent },
   { path: '', redirectTo: '/' + RoutesName.default, pathMatch: 'full' },
   { path: '**', redirectTo: '/' + RoutesName.default, pathMatch: 'full' }
 ];
